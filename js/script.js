a68a272d670bdb6b2673f480b4d98a5537120525
Andy Ball <932400399@bun2-206-11.local>
2019-08-02 10:02:50 +1200

    interval logging quakes within last minute

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
       dataType: 'json',
       success: function(jsonData){
         console.log('success!');
-      
+
         for (var i = 0; i < 10; i++) {
           console.log(jsonData.features[0].geometry.coordinates);
         }
@@ -38,12 +38,66 @@ $(document).ready(function(){
   $('#highIntensity').click(function(reported){
     mmi = 0;
     getEarthquakesByIntensity(mmi);
-    // for (var i = 0; i < 3; i++) {
-    //   mmi ++;
-    //   getEarthquakesByIntensity(mmi);
-    // }
+    for (var i = 0; i < 3; i++) {
+      mmi ++;
+      getEarthquakesByIntensity(mmi);
+    }
   })
-  //
+
+  getLastMinuteQuakes = () => {
+
+    const oneMinute = 60000;
+    const timeNow = Date.now();
+
+    let newTime;
+
+    let mmi = 0;
+
+    for (var i = 0; i < 10; i++) {
+      mmi ++;
+
+      $.ajax({
+        url: `https://api.geonet.org.nz/quake?MMI=${mmi}`,
+        type: 'GET',
+        dataType: 'json',
+        success: function(jsonData){
+          console.log('success!');
+          for (var i = 0; i < jsonData.features.length; i++) {
+            // console.log(jsonData.features[i].properties.time);
+            var quakeTime = Date.parse(jsonData.features[i].properties.time);
+
+            if ((timeNow - quakeTime) > oneMinute){
+              // console.log('quakeTime time is less than a minute before time now');
+              console.log(quakeTime);
+            }
+          }
+        },
+        error: function(jsonData){
+          console.log('something terrible has happened');
+        },
+      })
+    }
+  }
+
+  setInterval(function (){
+    // console.log('thirty seconds has passed');
+    getLastMinuteQuakes();
+
+
+  }, 30000)
+
+
+
+
+
+
+
+
+
+
+
+
+
   // function initMap() {
   //   var map = new google.maps.Map(document.getElementById('map'), {
   //     center: {lat: -40.90, lng: 174.88},
